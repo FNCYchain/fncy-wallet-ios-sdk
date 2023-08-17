5b0033d5e45610116fa14ad9d4eae5ed768a1aec
jh.park <jhpark27@nm-metaworld.com>
2023-08-17 11:15:38 +0900

    Recovery AppDelegate

--- Example/FncyWallet/AppDelegate.swift
+++ Example/FncyWallet/AppDelegate.swift
@@ -23,8 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        FncyWalletSDK.initSDK(apiKey: "qhswiamdxgs3vf478795tahvubrgw5rx",
-                              baseURL: "https://devx-apis.cubeapi.io")
+        FncyWalletSDK.initSDK(apiKey: "",
+                              baseURL: "")
         
         return true
     }
